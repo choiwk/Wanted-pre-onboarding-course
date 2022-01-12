292cec15c42b4acc8d596a04d845a52c3942da59
choiwk <dbk03053@gmail.com>
2022-01-12 19:15:20 +0900

    🔧  Main 페이지를 위한 폴더 생성 및 main.scss추가

---
+++ wanted_page/src/Pages/Main/Main.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-4 col-md-6 col-lg-4">
+            <div className="section">Columns</div>
+          </div>
+          <div className="col-sm-4 col-md-6 col-lg-4">
+            <div className="section">Columns</div>
+          </div>
+          <div className="col-sm-4 col-md-6 col-lg-4">
+            <div className="section">Columns</div>
+          </div>
+        </div>
+      </div>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus nulla
+        <i className="ic-alarm"></i>
+      </p>
+      <i className="ic-arrow"></i>
+    </>
+  );
+};
+
+export default Main;
