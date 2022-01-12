7b9459f053e8fe0e0c0384f87fe6f93e25dba60a
choiwk <dbk03053@gmail.com>
2022-01-12 21:50:39 +0900

    🔧  styles.scss 네이밍 변경에 의한 경로 수정

--- wanted_page/src/index.js
+++ wanted_page/src/index.js
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import Routes from './Routes';
-import '../src/styles/style.scss';
+import '../src/styles/styles.scss';
 import Nav from './Components/Nav/Nav';
 
 ReactDOM.render(
